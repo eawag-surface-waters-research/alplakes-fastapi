341f8653d682c50f15583cf45cde193b50a53ba7
JamesRunnalls <james.runnalls@eawag.ch>
2022-10-04 16:05:25 +0200

    Add data parsing from NetCDF files

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from fastapi.middleware.gzip import GZipMiddleware
 
 from app import meteoswiss
 
@@ -15,6 +16,9 @@ app = FastAPI(
         "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
     },
 )
+app.add_middleware(GZipMiddleware, minimum_size=1000)
+
+filesystem = "../filesystem"
 
 
 @app.get("/")
@@ -25,7 +29,8 @@ def welcome():
 
 @app.get("/meteoswiss/cosmo/reanalysis/{model}/{start_time}/{end_time}/{ll_lat}/{ll_lng}/{ur_lat}/{ur_lng}", tags=["Meteoswiss"])
 async def meteoswiss_cosmo_reanalysis(model: meteoswiss.CosmoReanalysis, start_time: int, end_time: int, ll_lat: float,
-                                      ll_lng: float, ur_lat: float, ur_lng: float):
+                                      ll_lng: float, ur_lat: float, ur_lng: float,
+                                      variables: list[str] = Query(default=["T_2M", "U", "V", "GLOB", "RELHUM_2M", "PMSL", "CLCT"])):
     """
     Weather data from MeteoSwiss COSMO reanalysis:
     - **cosmo**: select a COSMO product
@@ -38,13 +43,14 @@ async def meteoswiss_cosmo_reanalysis(model: meteoswiss.CosmoReanalysis, start_t
     - **ur_lat**: Latitude of upper right corner of bounding box (WGS 84)
     - **ur_lng**: Longitude of upper right corner of bounding box (WGS 84)
     """
-    meteoswiss.verify_cosmo_reanalysis(model, start_time, end_time, ll_lat, ll_lng, ur_lat, ur_lng)
-    return meteoswiss.get_cosmo_reanalysis(model, start_time, end_time, ll_lat, ll_lng, ur_lat, ur_lng)
+    meteoswiss.verify_cosmo_reanalysis(model, variables, start_time, end_time, ll_lat, ll_lng, ur_lat, ur_lng)
+    return meteoswiss.get_cosmo_reanalysis(filesystem, model, variables, start_time, end_time, ll_lat, ll_lng, ur_lat, ur_lng)
 
 
 @app.get("/meteoswiss/cosmo/forecast/{model}/{forecast_date}/{ll_lat}/{ll_lng}/{ur_lat}/{ur_lng}", tags=["Meteoswiss"])
-async def meteoswiss_cosmo_forecast(model: meteoswiss.CosmoForecast, forecast_date: str, ll_lat: float, ll_lng: float,
-                                    ur_lat: float, ur_lng: float):
+async def meteoswiss_cosmo_forecast(model: meteoswiss.CosmoForecast, forecast_date: str,
+                                    ll_lat: float, ll_lng: float, ur_lat: float, ur_lng: float,
+                                    variables: list[str] = Query(default=["T_2M", "U", "V", "GLOB", "RELHUM_2M", "PMSL", "CLCT"])):
     """
     Weather data from MeteoSwiss COSMO forecasts:
     - **cosmo**: select a COSMO product
@@ -56,6 +62,6 @@ async def meteoswiss_cosmo_forecast(model: meteoswiss.CosmoForecast, forecast_da
     - **ur_lat**: latitude of upper right corner of bounding box (WGS 84)
     - **ur_lng**: longitude of upper right corner of bounding box (WGS 84)
     """
-    meteoswiss.verify_cosmo_forecast(model, forecast_date, ll_lat, ll_lng, ur_lat, ur_lng)
-    return meteoswiss.get_cosmo_forecast(model, forecast_date, ll_lat, ll_lng, ur_lat, ur_lng)
+    meteoswiss.verify_cosmo_forecast(model, variables, forecast_date, ll_lat, ll_lng, ur_lat, ur_lng)
+    return meteoswiss.get_cosmo_forecast(filesystem, model, variables, forecast_date, ll_lat, ll_lng, ur_lat, ur_lng)
 
--- app/meteoswiss.py
+++ app/meteoswiss.py
@@ -1,7 +1,11 @@
 import os
-import netCDF4
+import json
+import numpy as np
 import xarray as xr
 from enum import Enum
+from datetime import datetime, timedelta
+from app.functions import NumpyArrayEncoder
+from fastapi import HTTPException
 
 
 class CosmoForecast(str, Enum):
@@ -9,15 +13,42 @@ class CosmoForecast(str, Enum):
     VNXZ32 = "VNXZ32"
 
 
-def verify_cosmo_forecast(model, forecast_date, ll_lat, ll_lng, ur_lat, ur_lng):
+def verify_cosmo_forecast(model, variables, forecast_date, ll_lat, ll_lng, ur_lat, ur_lng):
     return True
 
 
-def get_cosmo_forecast(model, forecast_date, ll_lat, ll_lng, ur_lat, ur_lng):
-    folder = os.path.join("../../filesystem/media/meteoswiss/cosmo", model)
-    ds = xr.open_mfdataset(os.path.join(folder, "{}.{}0000.nc".format(model, forecast_date)))
-    print(ds)
-    return "Getting data"
+def get_cosmo_forecast(filesystem, model, variables, forecast_date, ll_lat, ll_lng, ur_lat, ur_lng):
+    file = os.path.join(filesystem, "media/meteoswiss/cosmo", model, "{}.{}0000.nc".format(model, forecast_date))
+    if not os.path.isfile(file):
+        raise HTTPException(status_code=400, detail="Data not available for COSMO {} for the following date: {}".format(model, forecast_date))
+    output = {}
+    with xr.open_mfdataset(file) as ds:
+        bad_variables = []
+        for var in variables:
+            if var not in ds.variables.keys():
+                bad_variables.append(var)
+        if len(bad_variables) > 0:
+            raise HTTPException(status_code=400,
+                                detail="{} are bad variables for COSMO {}. Please select from: {}".format(
+                                    ", ".join(bad_variables), model, ", ".join(ds.keys())))
+
+        output["time"] = np.array(ds.variables["time"].values, dtype=str)
+        x, y = np.where(((ds.variables["lat_1"] >= ll_lat) & (ds.variables["lat_1"] <= ur_lat) & (ds.variables["lon_1"] >= ll_lng) & (ds.variables["lon_1"] <= ur_lng)))
+        x_min, x_max, y_min, y_max = min(x), max(x), min(y), max(y)
+        output["lat"] = ds.variables["lat_1"][x_min:x_max, y_min:y_max].values
+        output["lng"] = ds.variables["lon_1"][x_min:x_max, y_min:y_max].values
+        for var in variables:
+            if var in ds.variables.keys():
+                if ds.variables[var].dims == ('time', 'epsd_1', 'y_1', 'x_1'):
+                    data = ds.variables[var][:, 0, x_min:x_max, y_min:y_max].values
+                elif len(ds.variables[var].dims) == 5:
+                    data = ds.variables[var][:, 0, 0, x_min:x_max, y_min:y_max].values
+                else:
+                    data = []
+                output[var] = np.where(np.isnan(data), None, data)
+            else:
+                output[var] = []
+    return json.loads(json.dumps(output, cls=NumpyArrayEncoder))
 
 
 class CosmoReanalysis(str, Enum):
@@ -25,9 +56,45 @@ class CosmoReanalysis(str, Enum):
     VNXQ34 = "VNXQ34"
 
 
-def verify_cosmo_reanalysis(model, start_time, end_time, ll_lat, ll_lng, ur_lat, ur_lng):
+def verify_cosmo_reanalysis(model, variables, start_time, end_time, ll_lat, ll_lng, ur_lat, ur_lng):
     return True
 
 
-def get_cosmo_reanalysis(model, start_time, end_time, ll_lat, ll_lng, ur_lat, ur_lng):
-    return "Getting data"
+def get_cosmo_reanalysis(filesystem, model, variables, start_time, end_time, ll_lat, ll_lng, ur_lat, ur_lng):
+    folder = os.path.join(filesystem, "media/meteoswiss/cosmo", model)
+    start_time = datetime.fromtimestamp(start_time)
+    end_time = datetime.fromtimestamp(end_time)
+    files = [os.path.join(folder, "{}.{}0000.nc".format(model, (start_time+timedelta(days=x)).strftime("%Y%m%d")))
+             for x in range((end_time-start_time).days + 1)]
+    bad_files = []
+    for file in files:
+        if not os.path.isfile(file):
+            bad_files.append(file.split("/")[-1].split(".")[1][:8])
+    if len(bad_files) > 0:
+        raise HTTPException(status_code=400, detail="Data not available for COSMO {} for the following dates: {}".format(model, ", ".join(bad_files)))
+    output = {}
+    with xr.open_mfdataset(files) as ds:
+        bad_variables = []
+        for var in variables:
+            if var not in ds.variables.keys():
+                bad_variables.append(var)
+        if len(bad_variables) > 0:
+            raise HTTPException(status_code=400, detail="{} are bad variables for COSMO {}. Please select from: {}".format(", ".join(bad_variables), model, ", ".join(ds.keys())))
+        output["time"] = np.array(ds.variables["time"].values, dtype=str)
+        x, y = np.where(((ds.variables["lat_1"] >= ll_lat) & (ds.variables["lat_1"] <= ur_lat) & (
+                    ds.variables["lon_1"] >= ll_lng) & (ds.variables["lon_1"] <= ur_lng)))
+        x_min, x_max, y_min, y_max = min(x), max(x), min(y), max(y)
+        output["lat"] = ds.variables["lat_1"][x_min:x_max, y_min:y_max].values
+        output["lng"] = ds.variables["lon_1"][x_min:x_max, y_min:y_max].values
+        for var in variables:
+            if var in ds.variables.keys():
+                if ds.variables[var].dims == ('time', 'epsd_1', 'y_1', 'x_1'):
+                    data = ds.variables[var][:, 0, x_min:x_max, y_min:y_max].values
+                elif len(ds.variables[var].dims) == 5:
+                    data = ds.variables[var][:, 0, 0, x_min:x_max, y_min:y_max].values
+                else:
+                    data = []
+                output[var] = np.where(np.isnan(data), None, data)
+            else:
+                output[var] = []
+    return json.loads(json.dumps(output, cls=NumpyArrayEncoder))
